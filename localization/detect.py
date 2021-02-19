b39a9482dfec80257c5d3fb75220d6a6b21d2c86
sanjeev <sanjeevk1999@gmail.com>
2021-02-19 23:00:17 +0530

    localization

---
+++ localization/detect.py
@@ -0,0 +1,102 @@
+from core.config import cfg
+import os 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+from absl import app, flags, logging
+from absl.flags import FLAGS 
+import core.utils as utils
+from core.functions import crop
+from tensorflow.python.saved_model import tag_constants
+from PIL import Image
+import cv2 
+import numpy as np 
+from tensorflow.compat.v1 import ConfigProto 
+from tensorflow.compat.v1 import InteractiveSession 
+
+flags.DEFINE_list('images', './data/images/kite.jpg', 'path to input image')
+
+def main(_argv):
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = InteractiveSession(config=config)
+    # STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+    input_size = 416
+    images = FLAGS.images
+
+    # load model
+    saved_model_loaded = tf.saved_model.load(
+        './checkpoints/custom-416', tags=[tag_constants.SERVING])
+
+    # loop through images in list and run Yolov4 model on each
+    for count, image_path in enumerate(images, 1):
+        original_image = cv2.imread(image_path)
+        original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+
+        image_data = cv2.resize(original_image, (input_size, input_size))
+        image_data = image_data / 255.
+
+        # get image name by using split method
+        image_name = image_path.split('/')[-1]
+        image_name = image_name.split('.')[0]
+
+        images_data = []
+        for i in range(1):
+            images_data.append(image_data)
+        images_data = np.asarray(images_data).astype(np.float32)
+
+        infer = saved_model_loaded.signatures['serving_default']
+        batch_data = tf.constant(images_data)
+        pred_bbox = infer(batch_data)
+        for key, value in pred_bbox.items():
+            boxes = value[:, :, 0:4]
+            pred_conf = value[:, :, 4:]
+
+        # run non max suppression on detections
+        boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+            boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+            scores=tf.reshape(
+                pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+            max_output_size_per_class=50,
+            max_total_size=50,
+            iou_threshold=0.45,
+            score_threshold=0.50
+        )
+
+        # format bounding boxes from normalized ymin, xmin, ymax, xmax ---> xmin, ymin, xmax, ymax
+        original_h, original_w, _ = original_image.shape
+        bboxes = utils.format_boxes(boxes.numpy()[0], original_h, original_w)
+
+        # hold all detection data in one variable
+        pred_bbox = [bboxes, scores.numpy()[0], classes.numpy()[0],
+                    valid_detections.numpy()[0]]
+
+        # read in all class names from config
+        class_names = utils.read_class_names(cfg.YOLO.CLASSES)
+
+        # by default allow all classes in .names file
+        allowed_classes = list(class_names.values())
+
+        # draw image
+        image = utils.draw_bbox(original_image, pred_bbox, info=False,
+                                allowed_classes=allowed_classes, read_plate=False)
+
+        image = Image.fromarray(image.astype(np.uint8))
+        image.show()
+        image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+        cv2.imwrite('./detections/' + 'detection' + str(count) + '.png', image)
+
+        # # cropped image
+        # image = crop(cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB), pred_bbox)
+        # image = Image.fromarray(image.astype(np.uint8))
+        # image.show()
+        # image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+        # cv2.imwrite('./crop/' + 'detection' + str(count) + '.png', image)
+
+if __name__ == '__main__':
+    try:
+        app.run(main)
+    except SystemExit:
+        pass
